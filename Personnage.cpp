16ef8f074809bf721dd31c74050026cfc724e5c8
Axel Bruneaux <axel.bruneaux@logisneuf.com>
2018-07-03 16:00:15 +0200

    Class Personnage avec implementations des methods

---
+++ Personnage.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include "Personnage.h"
+
+using namespace std;
+
+Personnage::Personnage(string nomArme, int degatsArme) : m_vie(100), m_mana(100), m_nomArme("Épée rouillée"), m_degatsArme(10)
+{
+
+}
+
+void Personnage::recevoirDegats(int nbDegats)
+{
+    m_vie -= nbDegats;
+
+    if (m_vie < 0)
+    {
+        m_vie = 0;
+    }
+}
+
+void Personnage::attaquer(Personnage &cible)
+{
+    cible.recevoirDegats(m_degatsArme);
+}
+
+void Personnage::boirePotionDeVie(int quantitePotion)
+{
+    m_vie += quantitePotion;
+
+    if (m_vie > 100)
+    {
+        m_vie = 100;
+    }
+}
+
+void Personnage::changerArme(string nomNouvelleArme, int degatsNouvelleArme)
+{
+    m_nomArme = nomNouvelleArme;
+    m_degatsArme = degatsNouvelleArme;
+}
+
+bool Personnage::estVivant()
+{
+    return m_vie > 0;
+
+}
